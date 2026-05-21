67d45607d6d3a473932a25e6e21688f135094eee
dalemy-1 <9848014@gmail.com>
2026-05-21 08:45:44 +0800

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -5,7 +5,7 @@ const port = 3000;  // 选择一个端口
 
 app.get('/api/products', async (req, res) => {
   try {
-    const response = await axios.get('http://154.48.226.28:801/8ce7d1f0-8aa0-4da7-afe2-84653fbe52ea');
+    const response = await axios.get('http://154.48.226.28:5001/admin/Product/export_csv');
     res.send(response.data);
   } catch (error) {
     console.error(error);
